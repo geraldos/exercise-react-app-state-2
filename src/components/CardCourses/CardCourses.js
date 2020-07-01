2bb5e8f82b552a4cbdcba2aa57e70f50733e205b
geraldos <sepdwijaya18@gmail.com>
2020-07-01 15:36:07 +0700

    fix styling.......

--- src/components/CardCourses/CardCourses.js
+++ src/components/CardCourses/CardCourses.js
@@ -7,7 +7,7 @@ function CardCourses(props) {
         <div>
             <div className="column">
                 <div className="card">
-                <iframe width="100" height="100" src={props.iframe} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                <iframe width="100" height="100" title='Youtubes' src={props.iframe} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
 
                     <div className='text'>
                         <p className='description'>{props.cardsTitle}</p>
